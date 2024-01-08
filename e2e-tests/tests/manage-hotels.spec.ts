b0dd960763202906675248317a9d0191debae253
Shaun Hennedige <99944769+ShaunHennedige@users.noreply.github.com>
2024-01-08 11:10:14 +0530

    Update manage-hotels.spec.ts

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -11,8 +11,8 @@ test.beforeEach(async ({ page }) => {
 
   await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
 
-  await page.locator("[name=email]").fill("1@1.com");
-  await page.locator("[name=password]").fill("password123");
+  await page.locator("[name=email]").fill("admin@test.com");
+  await page.locator("[name=password]").fill("admin12345");
 
   await page.getByRole("button", { name: "Login" }).click();
 
@@ -22,12 +22,12 @@ test.beforeEach(async ({ page }) => {
 test("should allow user to add a hotel", async ({ page }) => {
   await page.goto(`${UI_URL}add-hotel`);
 
-  await page.locator('[name="name"]').fill("Test Hotel");
-  await page.locator('[name="city"]').fill("Test City");
-  await page.locator('[name="country"]').fill("Test Country");
+  await page.locator('[name="name"]').fill("Hilton Hotel, Colombo");
+  await page.locator('[name="city"]').fill("Colombo");
+  await page.locator('[name="country"]').fill("Sri Lanka");
   await page
     .locator('[name="description"]')
-    .fill("This is a description for the Test Hotel");
+    .fill("This is a description for the Hilton Hotel in Colombo, Sri Lanka");
   await page.locator('[name="pricePerNight"]').fill("100");
   await page.selectOption('select[name="starRating"]', "3");
 
@@ -36,8 +36,8 @@ test("should allow user to add a hotel", async ({ page }) => {
   await page.getByLabel("Free Wifi").check();
   await page.getByLabel("Parking").check();
 
-  await page.locator('[name="adultCount"]').fill("2");
-  await page.locator('[name="childCount"]').fill("4");
+  await page.locator('[name="adultCount"]').fill("100");
+  await page.locator('[name="childCount"]').fill("50");
 
   await page.setInputFiles('[name="imageFiles"]', [
     path.join(__dirname, "files", "1.png"),
@@ -51,9 +51,11 @@ test("should allow user to add a hotel", async ({ page }) => {
 test("should display hotels", async ({ page }) => {
   await page.goto(`${UI_URL}my-hotels`);
 
-  await expect(page.getByText("Dublin Getaways")).toBeVisible();
-  await expect(page.getByText("Lorem ipsum dolor sit amet")).toBeVisible();
-  await expect(page.getByText("Dublin, Ireland")).toBeVisible();
+  await expect(page.getByText("Hilton Hotel, Colombo")).toBeVisible();
+  await expect(
+    page.getByText("This is a description for the Hilton Hotel in Colombo, Sri Lanka")
+  ).toBeVisible();
+  await expect(page.getByText("Colombo, Sri Lanka")).toBeVisible();
   await expect(page.getByText("All Inclusive")).toBeVisible();
   await expect(page.getByText("£119 per night")).toBeVisible();
   await expect(page.getByText("2 adults, 3 children")).toBeVisible();
@@ -71,16 +73,16 @@ test("should edit hotel", async ({ page }) => {
   await page.getByRole("link", { name: "View Details" }).first().click();
 
   await page.waitForSelector('[name="name"]', { state: "attached" });
-  await expect(page.locator('[name="name"]')).toHaveValue("Dublin Getaways");
-  await page.locator('[name="name"]').fill("Dublin Getaways UPDATED");
+  await expect(page.locator('[name="name"]')).toHaveValue("Hilton Hotel, Colombo");
+  await page.locator('[name="name"]').fill("Hilton Hotel, Colombo UPDATED");
   await page.getByRole("button", { name: "Save" }).click();
   await expect(page.getByText("Hotel Saved!")).toBeVisible();
 
   await page.reload();
 
   await expect(page.locator('[name="name"]')).toHaveValue(
-    "Dublin Getaways UPDATED"
+    "Hilton Hotel, Colombo UPDATED"
   );
-  await page.locator('[name="name"]').fill("Dublin Getaways");
+  await page.locator('[name="name"]').fill("Hilton Hotel, Colombo");
   await page.getByRole("button", { name: "Save" }).click();
 });
